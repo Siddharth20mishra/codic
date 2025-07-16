fd0d2f947b78fb3c49b858841caeefbb6155a6c2
siddharth20mishra <sm8725086@gmail.com>
2025-07-16 15:28:41 +0530

    completed backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
+const aiRoutes = require('./routes/ai.routes')
 const app = express()
+app.use(express.json())
+
 app.get('/' , (req , res)=>{
     res.send('Hello codic');
 })
 
+app.use('/ai' , aiRoutes)
 module.exports = app
